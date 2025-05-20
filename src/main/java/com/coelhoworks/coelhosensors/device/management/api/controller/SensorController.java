f558128ad638a96d364f81400bc2b10d941f0a59
Wilson Coelho <wlfcoelho@gmail.com>
2025-05-20 00:32:52 -0300

    find by id e correcao do id para string

--- src/main/java/com/coelhoworks/coelhosensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/coelhoworks/coelhosensors/device/management/api/controller/SensorController.java
@@ -1,13 +1,16 @@
 package com.coelhoworks.coelhosensors.device.management.api.controller;
 
 import com.coelhoworks.coelhosensors.device.management.api.model.SensorInput;
+import com.coelhoworks.coelhosensors.device.management.api.model.SensorOutput;
 import com.coelhoworks.coelhosensors.device.management.common.IdGenerator;
 import com.coelhoworks.coelhosensors.device.management.domain.model.Sensor;
 import com.coelhoworks.coelhosensors.device.management.domain.model.SensorId;
 import com.coelhoworks.coelhosensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors")
@@ -16,9 +19,16 @@ public class SensorController {
 
     private final SensorRepository sensorRepository;
 
+    @GetMapping("{sensorId}")
+    public SensorOutput getOne(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return convertToModel(sensor);
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Sensor create(@RequestBody SensorInput input){
+    public SensorOutput create(@RequestBody SensorInput input){
         Sensor sensor = Sensor.builder()
                 .id(new SensorId(IdGenerator.genereteTSID()))
                 .name(input.getName())
@@ -29,6 +39,20 @@ public class SensorController {
                 .enabled(false)
                 .build();
 
-        return sensorRepository.saveAndFlush(sensor);
+        sensor = sensorRepository.saveAndFlush(sensor);
+
+        return convertToModel(sensor);
+    }
+
+    private SensorOutput convertToModel(Sensor sensor) {
+        return SensorOutput.builder()
+                .id(sensor.getId().getValue())
+                .name(sensor.getName())
+                .ip(sensor.getIp())
+                .location(sensor.getLocation())
+                .protocol(sensor.getProtocol())
+                .model(sensor.getModel())
+                .enabled(sensor.getEnabled())
+                .build();
     }
 }
