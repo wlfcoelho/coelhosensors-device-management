d8507009ff92e8c6781d25c67d5a1c02082a4d0d
Wilson Coelho <wlfcoelho@gmail.com>
2025-05-22 23:09:48 -0300

    integracao exception entra apis

--- src/main/java/com/coelhoworks/coelhosensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/coelhoworks/coelhosensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -3,8 +3,12 @@ package com.coelhoworks.coelhosensors.device.management.api.client.impl;
 import com.coelhoworks.coelhosensors.device.management.api.client.SensorMonitoringClient;
 import io.hypersistence.tsid.TSID;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
@@ -13,10 +17,20 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
 
   public SensorMonitoringClientImpl(RestClient.Builder builder) {
     this.restClient = builder.baseUrl("http://localhost:8082")
+            .requestFactory(generateClientHttpRequestFactory())
             .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
               throw new SensorMonitoringClientBadGatewayException();
             })
             .build();
+  }
+
+  private ClientHttpRequestFactory generateClientHttpRequestFactory() {
+    SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+    factory.setReadTimeout(Duration.ofSeconds(5));
+    factory.setReadTimeout(Duration.ofSeconds(3));
+
+    return factory;
   }
 
 
---
+++ src/main/java/com/coelhoworks/coelhosensors/device/management/api/config/web/ApiExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.coelhoworks.coelhosensors.device.management.api.config.web;
+
+import com.coelhoworks.coelhosensors.device.management.api.client.impl.SensorMonitoringClientBadGatewayException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+import java.net.URI;
+import java.nio.channels.ClosedChannelException;
+
+@RestControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler({
+          SocketTimeoutException.class,
+          ConnectException.class,
+          ClosedChannelException.class
+  })
+  public ProblemDetail handle(IOException e){
+    ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.GATEWAY_TIMEOUT);
+
+    problemDetail.setTitle("Gateway Timeout");
+    problemDetail.setDetail(e.getMessage());
+    problemDetail.setType(URI.create("/errors/gateway-timeout"));
+
+    return problemDetail;
+  }
+
+  @ExceptionHandler(SensorMonitoringClientBadGatewayException.class)
+  public ProblemDetail handle(SensorMonitoringClientBadGatewayException e){
+    ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_GATEWAY);
+
+    problemDetail.setTitle("Bad Gateway");
+    problemDetail.setDetail(e.getMessage());
+    problemDetail.setType(URI.create("/errors/bad-gateway"));
+
+    return problemDetail;
+
+  }
+}
